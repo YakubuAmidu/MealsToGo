f003e4e347c3a6127d6b860c534316b1071f75e8
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-05-22 04:06:26 -0700

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import React from "react";
+import * as React from "react";
 import { StatusBar, StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { Searchbar } from "react-native-paper";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <SafeAreaView style={styles.container}>
         <View style={styles.search}>
           <Text>Search</Text>
+          <Searchbar placeholder="Search" value="search" />
         </View>
         <View style={styles.list}>
           <Text>List</Text>
