3557d4b9bccbdd720742f32f1eba4efc144f9b7f
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-05-23 00:59:25 -0700

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,35 @@
-import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import React from "react";
+import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
 
 export default function App() {
   return (
     <>
-      <RestaurantsScreen />
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Text>search</Text>
+        </View>
+        <View style={styles.list}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
       <ExpoStatusBar style="auto" />
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+    backgroundColor: "green",
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "blue",
+  },
+});
