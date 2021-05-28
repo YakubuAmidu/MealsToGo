d2f39309eddd1ec491c7276d78561980da9661d5
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-05-28 01:30:53 -0700

    Initial commit

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,5 +1,4 @@
 import React, { useContext } from "react";
-import { View } from "react-native";
 import { Searchbar } from "react-native-paper";
 import { FlatList } from "react-native";
 import styled from "styled-components/native";
@@ -21,21 +20,24 @@ const RestaurantList = styled(FlatList).attrs({
   },
 })``;
 
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
+
+const LoadingContainer = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
+
 export const RestaurantsScreen = () => {
   const { isLoading, error, restaurants } = useContext(RestaurantsContext);
   return (
     <SafeArea>
       {isLoading && (
-        // eslint-disable-next-line react-native/no-inline-styles
-        <View style={{ position: "absolute", top: "50%", left: "50%" }}>
-          <ActivityIndicator
-            size={50}
-            // eslint-disable-next-line react-native/no-inline-styles
-            style={{ marginLeft: -25 }}
-            animating={true}
-            color={Colors.blue300}
-          />
-        </View>
+        <LoadingContainer>
+          <Loading size={50} animating={true} color={Colors.blue300} />
+        </LoadingContainer>
       )}
       <SearchContainer>
         <Searchbar />
