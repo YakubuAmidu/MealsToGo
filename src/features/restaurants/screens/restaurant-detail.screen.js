5da9ce4a13c814eb64bd5345f1799772dbe9e083
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-05-30 00:31:48 -0700

    Initial commit

---
+++ src/features/restaurants/screens/restaurant-detail.screen.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+import { SafeArea } from "../../../components/utility/safe-area.component";
+
+export const RestaurantDetailScreen = ({ route }) => {
+  const { restaurant } = route.name;
+  return (
+    <SafeArea>
+      <RestaurantInfoCard restaurant={restaurant} />
+    </SafeArea>
+  );
+};
