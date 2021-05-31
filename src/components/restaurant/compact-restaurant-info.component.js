87af3555d2e4a178366c0ec49c794402862abf77
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-05-31 01:30:55 -0700

    Initial commit

--- src/components/restaurant/compact-restaurant-info.component.js
+++ src/components/restaurant/compact-restaurant-info.component.js
@@ -1,10 +1,17 @@
 import React from "react";
 import styled from "styled-components";
 import WebView from "react-native-webview";
+import { Platform } from "react-native";
 
 import { Text } from "../typography/text.component";
 
 const CompactImage = styled.Image`
+  border-radius: 10px;
+  width: 120px;
+  height: 100px;
+`;
+
+const CompactWebView = styled(WebView)`
   border-radius: 10px;
   width: 120px;
   height: 100px;
@@ -16,10 +23,14 @@ const Item = styled.View`
   align-items: center;
 `;
 
+const isAndroid = Platform.OS === "android";
+
 export const CompactRestaurantInfo = ({ restaurant }) => {
+  const Image = isAndroid ? CompactWebView : CompactImage;
+
   return (
     <Item>
-      <CompactImage source={{ uri: restaurant.photos[0] }} />
+      <Image source={{ uri: restaurant.photos[0] }} />
       <Text center variant="caption" numberOfLines={3}>
         {restaurant.name}
       </Text>
