da215e2398d48df4a9d472f7342245609d7134f1
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-05-31 00:59:02 -0700

    Initial commit

---
+++ src/components/restaurant/compact-restaurant-info.component.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from 'styled-components';
+
+const CompactImage = styled.Image`
+ border-radius: 10px;
+ width: 120px;
+ height: 100px;
+`;
+
+const Item = styled.View`
+ padding: 10px;
+ max-width: 120px;
+ align-items: center;
+`;
+
+const CompactImage = styled.
+
+export const CompactRestaurantInfo = ({ restaurant }) => {};
