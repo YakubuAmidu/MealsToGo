efeef64e9a849f9a57520367a3cf8d5578638dc8
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-06-01 02:17:59 -0700

    Initial commit

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -1,9 +1,30 @@
 import React, { createContext, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const FavouritesContext = createContext();
 
 export const FavouritesContextProvider = ({ children }) => {
   const [favourites, setFavourites] = useState([]);
+
+  const saveFavourites = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem("@storage_key", jsonValue);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const loadFavourites = async () => {
+    try {
+      const value = await AsyncStorage.getItem("@storage_key");
+      if (value !== null) {
+        // Value previously stored
+      }
+    } catch (e) {
+      // Error reading value
+    }
+  };
 
   const add = (restaurant) => {
     setFavourites([...favourites, restaurant]);
