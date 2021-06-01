b998c154cf432eff6988f33b97f943232a454abe
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-05-31 19:33:22 -0700

    Initial commit

--- src/components/favourites/favourites-bar.component.js
+++ src/components/favourites/favourites-bar.component.js
@@ -1,3 +1,24 @@
 import React from "react";
-import { Text } from "react-native";
-export const FavouritesBar = () => <Text>Favourites Bar</Text>;
+import { ScrollView } from "react-native";
+import styled from "styled-components/native";
+import { Spacer } from "../spacer/spacer.component";
+import { CompactRestaurantInfo } from "../restaurant/compact-restaurant-info.component";
+
+const FavouritesWrapper = styled.View`
+  padding: 10px;
+`;
+
+export const FavouritesBar = ({ favourites }) => (
+  <FavouritesWrapper>
+    <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+      {favourites.map((restaurant) => {
+        const key = restaurant.name;
+        return (
+          <Spacer key={key} position="left" size="medium">
+            <CompactRestaurantInfo restaurant={restaurant} />
+          </Spacer>
+        );
+      })}
+    </ScrollView>
+  </FavouritesWrapper>
+);
