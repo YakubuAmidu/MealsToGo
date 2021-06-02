b0d97e20b26123af5988efa785ab9aa8969de1c9
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-06-02 05:13:13 -0700

    Initial commit

--- src/features/account/screens/login.screen.js
+++ src/features/account/screens/login.screen.js
@@ -1,11 +1,52 @@
-import React from "react";
-
-import { AccountBackground, AccountCover } from "../components/account.styles";
+import React, { useState, useContext } from "react";
+import {
+  AccountBackground,
+  AccountCover,
+  AccountContainer,
+  AuthButton,
+  AuthInput,
+} from "../components/account.styles";
+import { Spacer } from "../../../components/spacer/spacer.component";
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
 export const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { onLogin } = useContext(AuthenticationContext);
+
   return (
     <AccountBackground>
       <AccountCover />
+      <AuthenticationContext>
+        <AuthInput
+          label="E-mail"
+          value={email}
+          textContentType="emailAddress"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          onChangeText={(u) => setEmail(u)}
+        />
+        <Spacer size="large">
+          <AuthInput
+            label="Password"
+            value={password}
+            textContentType="password"
+            secureTextEntry
+            autoCapitalize="none"
+            secure
+            onChangeText={(p) => setPassword(p)}
+          />
+        </Spacer>
+        <Spacer size="large">
+          <AuthButton
+            icon="lock-open-outline"
+            mode="contained"
+            onPress={() => onLogin(email, password)}
+          >
+            Login
+          </AuthButton>
+        </Spacer>
+      </AuthenticationContext>
     </AccountBackground>
   );
 };
