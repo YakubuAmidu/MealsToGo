42014097cde12b817db83fda5b33018e7beeee49
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-06-02 03:44:05 -0700

    Initial commit

--- src/infrastructure/navigation/account.navigator.js
+++ src/infrastructure/navigation/account.navigator.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Text, View } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import { AccountScreen } from "../../features/account/screens/account.screen";
@@ -11,6 +12,6 @@ export const AccountNavigator = () => (
   <Stack.Navigator headerMode="none">
     <Stack.Screen name="Main" component={AccountScreen} />
     <Stack.Screen name="Login" component={LoginScreen} />
-    <Stack.Screen name="Register" component={RegisterScreen} />
+    <Stack.Screen name="Regiser" component={RegisterScreen} />
   </Stack.Navigator>
 );
