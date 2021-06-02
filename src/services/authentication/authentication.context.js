bcc62d08b412c4be9afeceec641a1c59e204993b
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-06-02 00:42:25 -0700

    Initial commit

--- src/services/authentication/authentication.context.js
+++ src/services/authentication/authentication.context.js
@@ -4,7 +4,21 @@ import * as firebase from "firebase";
 export const AuthenticationContext = createContext();
 
 export const AuthenticationContextProvider = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+
   return (
-    <AuthenticationContext.Provider>{children}</AuthenticationContext.Provider>
+    <AuthenticationContext.Provider
+      value={{
+        user,
+        isLoading,
+        error,
+        onLogin,
+      }}
+    >
+      {children}
+    </AuthenticationContext.Provider>
   );
 };
