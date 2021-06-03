bdbbcc6ce0a700338159754bda51ffef2e896c80
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-06-03 03:12:17 -0700

    Initial commit

--- src/features/account/screens/register.screen.js
+++ src/features/account/screens/register.screen.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 
-import { AccountBackground, AccountCover } from "../components/account.styles";
+import {
+  AccountBackground,
+  AccountCover,
+  AccountContainer,
+  AuthButton,
+  AuthInput,
+  ErrorContainer,
+  Title,
+} from "../components/account.styles";
+import { Text } from "../../../components/typography/text.component";
+import { Spacer } from "../../../components/spacer/spacer.component";
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
 export const RegisterScreen = () => {
   return (
