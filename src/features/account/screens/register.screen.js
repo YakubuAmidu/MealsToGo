44e2d09bf9b49407709c9d8998ad32e5d4713139
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-06-03 03:21:01 -0700

    Initial commit

--- src/features/account/screens/register.screen.js
+++ src/features/account/screens/register.screen.js
@@ -13,10 +13,25 @@ import { Text } from "../../../components/typography/text.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
-export const RegisterScreen = () => {
+export const RegisterScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatedPasswore, setRepeatedPassword] = useState("");
+  const { onRegister, error } = useContext(AuthenticationContext);
   return (
     <AccountBackground>
       <AccountCover />
+      <Title>Meals To Go</Title>
+      <AccountContainer>
+        <AuthInput
+          label="E-mail"
+          value={email}
+          textContentType="emailAddress"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          onChangeText={(u) => setEmail(u)}
+        />
+      </AccountContainer>
     </AccountBackground>
   );
 };
