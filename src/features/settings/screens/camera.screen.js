06fb0912c389f439efe526b004e46fb64035b69a
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-06-05 03:16:09 -0700

    Initial commit

--- src/features/settings/screens/camera.screen.js
+++ src/features/settings/screens/camera.screen.js
@@ -1,3 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Camera } from "expo-camera";
+import styled from "styled-components/native";
 
-export const CameraScreen = () => null;
+const ProfileCamera = styled(Camera)`
+  width: 100%;
+  height: 100%;
+`;
+
+export const CameraScreen = () => {
+  const cameraRef = useRef();
+
+  return (
+    <ProfileCamera
+      ref={(camera) => (cameraRef.current = camera)}
+      type={Camera.Constants.Type.front}
+    >
+      j
+    </ProfileCamera>
+  );
+};
